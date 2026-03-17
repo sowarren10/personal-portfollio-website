ff51605fafe09c6e92c76c7977fd993bd4784042
Shaquille Warren <your.github.email@example.com>
2026-03-17 03:44:00 -0400

    Add random greeting generator function

--- New folder/hello-world.py
+++ New folder/hello-world.py
@@ -1,3 +1,19 @@
 print("Hello, World!")
-print("Learning about commits today!")
+import random
+
+greetings = [
+    "Hello, Git!",
+    "Greetings, developer!",
+    "Welcome to branching!"
+    "Hi there, coding friend!",
+    "Happy coding!"
+]
+
+def get_random_greeting():
+    return random.choice(greetings)
+
+print(get_random_greeting())
+pring("Learning about branches today!")
+
+print("Learning about commits today!"
 print("Code is fun. When you have to explain it, it's bad!")
